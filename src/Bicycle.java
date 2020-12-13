f019b73201a8971477a610f11c585be8942b0b46
sim <chinnapak.21@mail.kmutt.ac.th>
2020-12-14 02:07:51 +0700

    new

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -25,10 +25,6 @@ public class Bicycle {
         return cost;
     }
 
-    public void setCost(double cost) {
-        this.cost = cost;
-    }
-
     @Override
     public String toString() {
         return "ID=" + getId_bi() + ", type=" + getType() + ", price=" + getPrice() + " Bath";
